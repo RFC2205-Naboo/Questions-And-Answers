0e1cd9c1532789b3ea862bd87e666f521ceaa865
David Fan <davidlfan@gmail.com>
2022-08-03 23:38:56 -0400

    edits

--- questions-and-answers-back-end/Server/controllers.js
+++ questions-and-answers-back-end/Server/controllers.js
@@ -2,8 +2,48 @@ const pool = require("../Database")
 
 const getQuestions = async (req, res) => {
   try {
-    console.log(req.params)
-    const allQuestions = await pool.query("SELECT * FROM question LIMIT 10");
+    let productIdString = req.query.product_id;
+    let productIdNum = parseInt(req.query.product_id, 10);
+    let count = parseInt(req.query.count, 10) || 5;
+    var page = parseInt(req.query.page, 10) - 1 || 1;
+    page = page * count;
+    const allQuestions = await pool.query(
+    `SELECT $1 AS product_id,
+    (SELECT json_agg(row_to_json(t)) AS results
+    FROM (
+      SELECT
+      question.id AS question_id,
+      question.body AS question_body,
+      question.date_written AS question_date,
+      question.asker_name AS asker_name,
+      question.reported AS reported,
+      (
+        SELECT json_object_agg(id, row_to_json(t)) AS answers
+        FROM (
+          SELECT
+          answer.id AS id,
+          answer.body AS body,
+          answer.date_written AS date,
+          answer.answerer_name AS answerer_name,
+          answer.helpful AS helpfulness,
+          (
+            SELECT json_agg(row_to_json(t)) AS photos
+            FROM (
+              SELECT answer_photo.id AS id,
+              answer_photo.url AS url
+              FROM answer_photo
+              WHERE answer_photo.answer_id = answer.id
+            ) t
+          )
+            FROM answer LEFT JOIN answer_photo ON answer.id = answer_photo.answer_id
+            WHERE answer.question_id = question.id GROUP BY answer.id ORDER BY date ASC
+          ) t
+        ) FROM question
+        WHERE question.product_id = $2
+        LIMIT $4
+        OFFSET $5
+      ) t)
+    FROM question WHERE question.product_id = $3 GROUP BY question.product_id;`, [productIdString, productIdNum, productIdNum, count, page]);
     res.json(allQuestions.rows);
   } catch (error) {
     console.error(error.message)
@@ -13,7 +53,43 @@ const getQuestions = async (req, res) => {
 
 const getAnswers = async (req, res) => {
   try {
-    const answers = await pool.query("SELECT * FROM answer WHERE answer.question_id = $1 LIMIT 10", [req.params.questions_id])
+    let questionIdString = req.params.questions_id;
+    let questionIdNum = parseInt(req.params.questions_id, 10);
+    let count = parseInt(req.query.count, 10) || 5;
+    let pageCount = parseInt(req.query.page, 10) || 1;
+    let pageOffset = (pageCount - 1)* count;
+    const answers = await pool.query(
+      `SELECT
+      $1 AS question,
+      $2 AS page,
+      $3 AS count,
+      (
+        SELECT json_agg(row_to_json(t)) AS results
+          FROM (
+            SELECT
+            answer.id AS answer_id,
+            answer.body AS body,
+            answer.date_written AS date,
+            answer.answerer_name AS answerer_name,
+            answer.helpful AS helpfulness,
+            (
+              SELECT json_agg(row_to_json(t)) AS photos
+                FROM (
+                  SELECT answer_photo.id AS id,
+                  answer_photo.url AS url
+                  FROM answer_photo
+                  WHERE answer_photo.answer_id = answer.id
+                ) t
+            )
+            FROM answer
+            LEFT JOIN answer_photo ON answer.id = answer_photo.answer_id
+            WHERE answer.question_id = $4
+            GROUP BY answer.id
+            ORDER BY date ASC
+            LIMIT $5
+            OFFSET $6
+          ) t
+      );`, [questionIdString, pageCount, count, questionIdNum, count, pageOffset])
     res.json(answers.rows);
   } catch (error) {
     console.error(error.message);
--- questions-and-answers-back-end/Server/qaDB.sql
+++ questions-and-answers-back-end/Server/qaDB.sql
@@ -6,36 +6,66 @@ CREATE TABLE question(
   id SERIAL PRIMARY KEY,
   product_id INT,
   body VARCHAR(1000),
-  date_written TEXT,
+  date_written BIGINT,
   asker_name VARCHAR(60),
   asker_email VARCHAR(60),
-  reported INT,
+  reported SMALLINT,
   helpful INT
-  -- reported is an int with a boolean 0 or 1 binary
-  -- Helpful is a count of number of likes
-  -- Need to figure out how to transform the date_written
   );
 
 COPY "question" from '/Users/T-Spoon/SDC - Project Naboo/questions-and-answers-back-end/Atelier Data Set/questions.csv' DELIMITER ',' CSV HEADER;
+
+-- Adding the new column
+ALTER TABLE question ADD date_written_placeholder TEXT;
+
+-- Updating the question
+UPDATE question
+SET date_written_placeholder = TO_CHAR(to_timestamp(date_written / 1000), 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"');
+
+-- Dropping the table
+ALTER TABLE question RENAME COLUMN date_written TO date_written_placeholder_2;
+ALTER TABLE question RENAME COLUMN date_written_placeholder TO date_written;
+ALTER TABLE question DROP COLUMN date_written_placeholder_2;
+
+-- recasting the reported
+ALTER TABLE question ALTER COLUMN reported DROP DEFAULT;
+ALTER TABLE question ALTER reported TYPE bool USING CASE WHEN reported = 0 THEN FALSE ELSE TRUE END;
+ALTER TABLE question ALTER COLUMN reported SET DEFAULT FALSE;
+
+
 
 CREATE TABLE answer(
   id SERIAL PRIMARY KEY,
   question_id INT,
   body VARCHAR(1000),
-  date_written TEXT,
+  date_written BIGINT,
   answerer_name VARCHAR(60),
   answerer_email VARCHAR(60),
-  reported INT,
+  reported SMALLINT,
   helpful INT,
   CONSTRAINT fk_question
     FOREIGN KEY (question_id)
       REFERENCES "question"(id)
-  -- Make question_Id a foreign key
-  -- Find the correct data types for DATE and BOOLEAN
-  -- Figure out how to run this sql file in the terminal to import all the files
 );
 
 COPY "answer" from '/Users/T-Spoon/SDC - Project Naboo/questions-and-answers-back-end/Atelier Data Set/answers.csv' DELIMITER ',' CSV HEADER;
+
+-- Adding the new column
+ALTER TABLE answer ADD date_written_placeholder TEXT;
+
+-- Updating the question
+UPDATE answer
+SET date_written_placeholder = TO_CHAR(to_timestamp(date_written / 1000), 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"');
+
+-- Dropping the table
+ALTER TABLE answer RENAME COLUMN date_written TO date_written_placeholder_2;
+ALTER TABLE answer RENAME COLUMN date_written_placeholder TO date_written;
+ALTER TABLE answer DROP COLUMN date_written_placeholder_2;
+
+-- recasting the reported
+ALTER TABLE answer ALTER COLUMN reported DROP DEFAULT;
+ALTER TABLE answer ALTER reported TYPE bool USING CASE WHEN reported = 0 THEN FALSE ELSE TRUE END;
+ALTER TABLE answer ALTER COLUMN reported SET DEFAULT FALSE;
 
 
 CREATE TABLE answer_photo(
@@ -45,7 +75,6 @@ CREATE TABLE answer_photo(
   CONSTRAINT fk_answer
     FOREIGN KEY (answer_id)
       REFERENCES "answer"(id)
-  -- Make answer_id a foreign key
 );
 
 COPY "answer_photo" from '/Users/T-Spoon/SDC - Project Naboo/questions-and-answers-back-end/Atelier Data Set/answers_photos.csv' DELIMITER ',' CSV HEADER;
